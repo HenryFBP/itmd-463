2c4db9309361615e20882fdd4f32a109b5bb0687
HenryFBP <HenryFBP@gmail.com>
2018-08-27 20:51:42 -0500

    add file-making web form example

---
+++ WebApplication2/WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Web.UI;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+
+            }
+            else
+            {
+
+                try
+                {
+                    using (StreamWriter sw = System.IO.File.AppendText(Server.MapPath("/App_Data/foo.txt")))
+                    {
+                        sw.Write($"WebForm1 accessed at {System.DateTime.UtcNow}.\n");
+
+                        sw.Flush();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+            }
+        }
+    }
+}
