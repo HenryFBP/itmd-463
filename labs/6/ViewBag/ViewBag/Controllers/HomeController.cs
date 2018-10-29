4101a92af8a41cf5e86e327b14a5e59dab80e25c
HenryFBP <HenryFBP@gmail.com>
2018-10-28 21:18:22 -0500

    finish lab 6

---
+++ labs/6/ViewBag/ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ViewBagLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.FirstName = "Henry";
+            ViewBag.LastName = "Post";
+            ViewBag.Course = "ITMD463";
+
+            ViewBag.Time = DateTime.Now.ToString();
+
+            return View();
+        }
+    }
+}
