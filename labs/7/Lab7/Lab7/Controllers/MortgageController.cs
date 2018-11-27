5fff23f247345a83eb3227eb9b79e080913bc243
Henry Post <HenryFBP@gmail.com>
2018-11-27 13:11:32 -0600

    add mortgageController, mortgageInfo, etc.

---
+++ labs/7/Lab7/Lab7/Controllers/MortgageController.cs
@@ -0,0 +1,33 @@
+﻿using Lab7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab7.Controllers
+{
+    public class MortgageController : Controller
+    {
+        // GET: Mortgage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(float cost, float principle, float interestRate, float durationYears, float monthlyPayment)
+        {
+            MortgageResult mortgageResult = new MortgageResult();
+
+            return View("IndexResult", mortgageResult);
+        }
+
+        [HttpGet] 
+        public ActionResult IndexResult(MortgageResult mortgageResult)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ labs/7/Lab7/Lab7/Models/MortgageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab7.Models
+{
+    public class MortgageInfo
+    {
+        public double Cost { get; set; }
+        public double Principle { get; set; }
+        public double InterestRate { get; set; }
+        public double DurationYears { get; set; }
+        public double MonthlyPayment { get; set; }
+    }
+}
